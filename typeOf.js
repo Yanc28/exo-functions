cfe0c8578d41c30153713f0437816241219dc851
Yanc28 <yanisbouabdellahw@gmail.com>
2021-03-03 23:19:48 +0100

    done

---
+++ typeOf.js
@@ -0,0 +1,10 @@
+/* Ecrire une fonction `typeOf` qui prend comme paramètre une variable,
+ et qui retourne une string qui sera le nom du type de cette variable passée en paramètre.*/
+
+
+function typeOf(prenom) {
+  return `Bienvenue ${prenom} à la maison`
+}
+
+prenom = 'Yanis'
+console.log(`Bienvenue ${prenom} à la maison`)
