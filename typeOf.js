0e8edc0412602d0a8ebe1ee3c3766b7759a1b974
Yanc28 <yanisbouabdellahw@gmail.com>
2021-03-04 14:43:44 +0100

    done

--- typeOf.js
+++ typeOf.js
@@ -1,10 +1,9 @@
 /* Ecrire une fonction `typeOf` qui prend comme paramètre une variable,
  et qui retourne une string qui sera le nom du type de cette variable passée en paramètre.*/
 
-
-function typeOf(prenom) {
-  return `Bienvenue ${prenom} à la maison`
+const typeOf = (myVar) => {
+  return typeof myVar
 }
 
-prenom = 'Yanis'
-console.log(`Bienvenue ${prenom} à la maison`)
+console.log(typeOf(1)) // output: 'number'
+console.log(typeOf([1, 2, 3])) // output: 'object'
